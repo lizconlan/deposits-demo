78589e1a76706ba943584b9a4f7ae43a1b9f6a38
lizconlan <lizconlan@gmail.com>
2010-11-05 11:57:11 +0000

    better use of couch views; removed unused views

--- db/views/years/map.js
+++
@@ -1,3 +0,0 @@
-function(doc) {
-    emit(doc.year, [doc.year]);
-}
--- db/views/years/reduce.js
+++
@@ -1,9 +0,0 @@
-var years = "";
-var count = 0;
-
-function(keys, values) {
-    for (var i=0; i<values.length; i++) {
-	count++;
-    }
-    return count;
-}
