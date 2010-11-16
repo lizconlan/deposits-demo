dd8b694b6bd8a3d9bfbccec74d56d15c15c06ac8
Robert Brook <mail@robertbrook.com>
2010-11-16 11:44:01 +0000

    favicon path

--- server.rb
+++ server.rb
@@ -85,6 +85,10 @@ get '/tags' do
   "some tags"
 end
 
+get '/favicon.ico' do
+  "http://www.parliament.uk/favicon.ico"
+end
+
 # "by tag" page, with pagination
 # get '/tags/:tag' and '/tags/:tag/page/:page'
 get %r{^\/tags\/([^\/]*)(?:\/page\/(\d+))?\/?$} do |tag, page|
